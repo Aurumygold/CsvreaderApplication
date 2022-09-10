95316963fe1b742ce10ec415a3a4b4ada8d03c9b
Pablo <pabcaten@outlook.es>
2022-09-10 02:30:06 +0200

    clase main de testing

--- src/test/java/com/example/mercadona/CsvreaderApplicationTests.java
+++ src/test/java/com/example/mercadona/CsvreaderApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.mercadona;
 
+import com.example.mercadona.processor.FacturasItemProcessorTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CsvreaderApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		FacturasItemProcessorTests facturasItemProcessorTests = new FacturasItemProcessorTests();
+		facturasItemProcessorTests.setUp();
+		facturasItemProcessorTests.testTratamientoFactura();
+		facturasItemProcessorTests.testTratamientoFecha();
+		facturasItemProcessorTests.testTratamientoImporte();
+		facturasItemProcessorTests.testTratamientoFechaKo();
+		facturasItemProcessorTests.testTratamientoImporteKo();
 	}
 
 }
